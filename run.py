36d98d12e77153eaffddf5241e0626eedb78fcf7
phantomchzo-ui <224369715+phantomchzo-ui@users.noreply.github.com>
2026-04-01 23:04:00 +0500

    commit

--- run.py
+++ run.py
@@ -1,17 +1,38 @@
 import asyncio
-
 from aiogram import Dispatcher, Bot
-
+from aiohttp import web
 from app.config import settings
 from app.handlers.router import router
 from app.handlers.payment import router_payment
+
+
+async def handle(request):
+    """Минимальный веб-сервер для Render"""
+    return web.Response(text="Bot is running")
+
+
+async def start_web_server():
+    """Запуск простого aiohttp сервера на порту 10000"""
+    app = web.Application()
+    app.add_routes([web.get("/", handle)])
+    runner = web.AppRunner(app)
+    await runner.setup()
+    site = web.TCPSite(runner, "0.0.0.0", 10000)  # Render требует любой порт
+    await site.start()
+    print("Web server started on port 10000")
 
 
 async def main():
     dp = Dispatcher()
     bot = Bot(token=settings.TOKEN)
     dp.include_routers(router, router_payment)
-    await dp.start_polling(bot)
+
+    # Запускаем фейковый веб-сервер параллельно с ботом
+    await asyncio.gather(
+        dp.start_polling(bot),
+        start_web_server()
+    )
+
 
 try:
     if __name__ == '__main__':
